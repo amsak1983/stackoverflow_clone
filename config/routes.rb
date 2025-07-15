a3ea9edd83824aa665fe24d6fd486f121569f077
amsak1983 <amsak1983@yandex.by>
2025-07-16 00:09:09 +0300

    chore: rubocop -a

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "questions#index"
-  
-  resources :questions, only: [:index, :new, :create, :show] do
-    resources :answers, only: [:create], shallow: true
+
+  resources :questions, only: [ :index, :new, :create, :show ] do
+    resources :answers, only: [ :create ], shallow: true
   end
 end
