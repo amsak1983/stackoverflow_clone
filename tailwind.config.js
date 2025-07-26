43e7b40c81a85f5a55673c9fb1862f3524e6f0ca
amsak1983 <amsak1983@yandex.by>
2025-07-26 04:35:27 +0300

    feat: integrate Tailwind CSS with Rails application setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,15 @@ module.exports = {
     './app/views/**/*.{html,html.erb,erb}',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
+    './app/assets/stylesheets/**/*.css',
     './node_modules/flowbite/**/*.js'
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+      },
+    },
   },
   plugins: [
     require('flowbite/plugin')
