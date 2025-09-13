aff28e2382edcc92f03b8acefd3305a6b9bf8cc3
amsak1983 <amsak@yandex.by>
2025-09-13 12:03:48 +0300

    tests: add rspecs

---
+++ spec/models/answer_search_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Answer, type: :model do
+  describe ".search_simple" do
+    let(:query) { "explain" }
+
+    it "returns none when query is blank" do
+      expect(described_class.search_simple("").records).to eq([])
+    end
+
+    it "delegates to __elasticsearch__.search with match on body" do
+      es = double("ESResult", records: [])
+      expect(described_class.__elasticsearch__).to receive(:search).with(
+        hash_including(
+          query: hash_including(
+            match: hash_including(
+              body: query
+            )
+          )
+        )
+      ).and_return(es)
+
+      expect(described_class.search_simple(query).records).to eq([])
+    end
+  end
+end
