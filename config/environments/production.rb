ab69fbf6084bcfd2365f9dae9e3339c1ddbb619f
amsak1983 <amsak@yandex.by>
2025-10-14 03:53:27 +0300

    default

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
   # Mailer configuration
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.deliver_later_queue_name = :mailers
+  config.action_mailer.deliver_later_queue_name = :default
   
   # Set host to be used by links generated in mailer templates.
   config.action_mailer.default_url_options = { 
